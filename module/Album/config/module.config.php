9a80f24708245b9d267a6eabb9da0f0a35778ebc
Puneet <puneetkhurana83@gmail.com>
2017-05-16 16:40:19 +0530

    First commit

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Album;
+use Zend\Router\Http\Segment;
+use Album\Controller\AlbumController;
+
+return [
+    // The following section is new and should be added to your file:
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => \Zend\Router\Http\Literal::class,
+                'options' => [
+                    'route'    => '/',
+                    'defaults' => [
+                        'controller' => AlbumController::class, // <-- change here
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
